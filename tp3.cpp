21b5680742f580584662e392e919c5c1e11969b1
AngelV1107 <angelleandro@live.com>
2019-03-28 19:26:23 -0300

    Agregado funciones de carga y lectura de datos

--- tp3.cpp
+++ tp3.cpp
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-enum TRaza{Orco, Humano, Mago, Enano, Elfo}
-char Nombres[6][10]={"ale", "b", "c", "d", "e"}
-char Apellidos[6][10]={"f", "g", "hormiga", "i", "j"}
+enum TRaza{Orco, Humano, Mago, Enano, Elfo};
+char Nombres[6][10]={"jorgito", "b", "c", "d", "e", "olaf"};
+char Apellidos[6][10]={"f", "g", "hormiga", "i", "j"};
 
 typedef struct TDatos {
 	TRaza Raza; //nota 1
-	Char *ApellidoNombre; //nota 2
+	char *ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
-	double Salud://100
+	double Salud;//100
 } TDatos;
 
 typedef struct TCaracteristicas{
@@ -21,13 +23,95 @@ typedef struct TCaracteristicas{
 } TCaracteristicas;
 
 typedef struct TPersonaje {
-	TDatos * DatosPersonales
-	TCaracteristicas * Caracteristicas
+	TDatos * DatosPersonales;
+	TCaracteristicas * Caracteristicas;
 } TPersonaje;
 
-
+TDatos cargaDatos ();
+void mostrarDatos (TDatos datos);
 
 int main (void) {
+	TDatos pj;
+
+	srand(time(NULL));
+
+	pj = cargaDatos();
+
+	mostrarDatos(pj);
 
 	return 0;
+}
+
+TDatos cargaDatos () {
+
+	TDatos datos;
+	enum TRaza raza;
+
+	switch(rand()%5) {
+		case 0: 
+			raza = Orco;
+			break;
+		case 1: 
+			raza = Humano;
+			break;
+		case 2: 
+			raza = Enano;
+			break;
+		case 3: 
+			raza = Mago;
+			break;
+		case 4: 
+			raza = Elfo;
+			break;
+
+		default: Humano;
+	}
+
+	datos.Raza = raza;
+
+	datos.ApellidoNombre = (char *) malloc (10);
+
+	strcpy((datos.ApellidoNombre), Nombres[rand()%6]);
+
+	datos.edad = rand()%300;
+
+	datos.Salud = (double)100;
+
+
+	return datos;
+}
+
+
+void mostrarDatos (TDatos datos) {
+
+
+printf("Raza: ");
+
+switch(datos.Raza) {
+		case 0: 
+			printf("%s\n", "Orco");
+			break;
+		case 1: 
+			printf("%s\n", "Humano");
+			break;
+		case 2: 
+			printf("%s\n", "Enano");
+			break;
+		case 3: 
+			printf("%s\n", "Mago");
+			break;
+		case 4: 
+			printf("%s\n", "Elfo");
+			break;
+
+		default: printf("%s\n", "Humano");
+	}
+
+
+
+	
+	printf("Nombre: %s\n", datos.ApellidoNombre);
+	printf("Edad: %d\n", datos.edad);
+	printf("Salud: %.2lf\n", datos.Salud);
+	return;
 }
