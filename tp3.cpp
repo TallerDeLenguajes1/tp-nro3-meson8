8177a3395b0b302264516831a55cc8460bb79220
Leonel Herrera - Lic. Informatica <leonelherreradkno@gmail.com>
2019-03-28 19:07:31 -0300

    Agregando la parte del grupo 2

--- tp3.cpp
+++ tp3.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-enum TRaza{Orco, Humano, Mago, Enano, Elfo}
+/*enum TRaza{Orco, Humano, Mago, Enano, Elfo}
 char Nombres[6][10]={"ale", "b", "c", "d", "e"}
-char Apellidos[6][10]={"f", "g", "hormiga", "i", "j"}
+char Apellidos[6][10]={"f", "g", "hormiga", "i", "j"}*/
 
-typedef struct TDatos {
+/*typedef struct TDatos {
 	TRaza Raza; //nota 1
 	Char *ApellidoNombre; //nota 2
 	int edad; //entre 0 a 300
 	double Salud://100
-} TDatos;
+} TDatos;*/
 
 typedef struct TCaracteristicas{
 	int velocidad;// 1 a 10
@@ -20,16 +21,34 @@ typedef struct TCaracteristicas{
 	int Armadura; //1 a 10
 } TCaracteristicas;
 
-typedef struct TPersonaje {
-	TDatos * DatosPersonales
-	TCaracteristicas * Caracteristicas
-} TPersonaje;
+/*typedef struct TPersonaje {
+	TDatos * DatosPersonales;
+	TCaracteristicas * Caracteristicas;
+} TPersonaje;*/
 
 
-
-int main (void) {
-
+void Cargar_Carac(TCaracteristicas *puntero);
+void Mostrar_Carac(TCaracteristicas *puntero);
+int main () {
+	TCaracteristicas *puntero;
+	puntero = (TCaracteristicas*)malloc(sizeof(TCaracteristicas));
+	Cargar_Carac(puntero);
+	Mostrar_Carac(puntero);
 	return 0;
 }
 
+void Cargar_Carac(TCaracteristicas *puntero){
+	puntero->velocidad = 1+rand()%(11-1);
+	puntero->destreza = 1+rand()%(6-1);
+	puntero->fuerza = 1+rand()%(11-1);
+	puntero->Nivel = 1+rand()%(11-1);
+	puntero->Armadura = 1+rand()%(11-1);
+}
+void Mostrar_Carac(TCaracteristicas *puntero){
+	printf("Velocidad: %d\n", puntero->velocidad );
+	printf("Destreza: %d\n", puntero->destreza);
+	printf("Fuerza: %d\n", puntero->fuerza);
+	printf("Nivel: %d\n", puntero->Nivel);
+	printf("Armadura: %d\n", puntero->Armadura);
+}
 //grupo2
