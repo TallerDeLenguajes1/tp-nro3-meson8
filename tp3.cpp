545d887504c60df2203e372bd5c957d6dfaf402f
AngelV1107 <angelleandro@live.com>
2019-04-04 19:46:42 -0300

    Agregada funcion de pelea

--- tp3.cpp
+++ tp3.cpp
@@ -18,8 +18,8 @@ typedef struct TCaracteristicas{
 	int velocidad;// 1 a 10
 	int destreza; //1 a 5
 	int fuerza;//1 a 10
-	int Nivel; //1 a 10
-	int Armadura; //1 a 10
+	int nivel; //1 a 10
+	int armadura; //1 a 10
 } TCaracteristicas;
 
 typedef struct TPersonaje {
@@ -35,6 +35,8 @@ void Mostrar_Carac(TCaracteristicas *carac);
 
 TPersonaje cargaPJ (void);
 void muestraPJ (TPersonaje pj);
+
+void peleaPJ(TPersonaje *p1, TPersonaje *p2);
 
 // ___________________________________________________
 //                      MAIN
@@ -69,6 +71,11 @@ int main (void) {
 		muestraPJ(arrayPJ[i]);
 		printf("\n");
 	}
+
+
+	printf("PJ1 vs PJ2\n");
+	peleaPJ(&arrayPJ[0], &arrayPJ[1]);
+
 
 	return 0;
 }
@@ -156,8 +163,8 @@ TCaracteristicas* Cargar_Carac(void){
 	carac->velocidad = 1+rand()%(11-1);
 	carac->destreza = 1+rand()%(6-1);
 	carac->fuerza = 1+rand()%(11-1);
-	carac->Nivel = 1+rand()%(11-1);
-	carac->Armadura = 1+rand()%(11-1);
+	carac->nivel = 1+rand()%(11-1);
+	carac->armadura = 1+rand()%(11-1);
 
 	return carac;
 }
@@ -165,8 +172,8 @@ void Mostrar_Carac(TCaracteristicas *carac){
 	printf("Velocidad: %d\n", carac->velocidad );
 	printf("Destreza: %d\n", carac->destreza);
 	printf("Fuerza: %d\n", carac->fuerza);
-	printf("Nivel: %d\n", carac->Nivel);
-	printf("Armadura: %d\n", carac->Armadura);
+	printf("Nivel: %d\n", carac->nivel);
+	printf("Armadura: %d\n", carac->armadura);
 }
 
 
@@ -182,5 +189,79 @@ TPersonaje cargaPJ (void) {
 void muestraPJ (TPersonaje pj) {
 	mostrarDatos(pj.DatosPersonales);
 	Mostrar_Carac(pj.Caracteristicas);
+	return;
+}
+
+
+void peleaPJ(TPersonaje *p1, TPersonaje *p2) {
+	int i;
+	int resultado; //1 si gana PJ1, 2 si gana PJ2, 0 si hay empate
+	int podrDisp, podrDef, maxDP;
+	double DP, efDisp, valorAtk;
+	TPersonaje *pA, *pB;
+	TCaracteristicas caracA, caracB;
+
+	maxDP = 5000;
+	resultado = 0;
+
+	for(i=0; i<6;i++) {
+		//Turno de PJ1 cuando i es par
+		//Turno de PJ2 cuando i es impar
+		if (i%2 == 0) {
+			pA = p1;
+			pB = p2;
+			caracA = *(pA->Caracteristicas);
+			caracB = *(pB->Caracteristicas);
+		}
+		else {
+			pA = p2;
+			pB = p1;
+			caracA = *(p2->Caracteristicas);
+			caracB = *(p1->Caracteristicas);
+		}
+
+		podrDisp = caracA.destreza * caracA.fuerza * caracA.nivel;
+		efDisp = (double) (1 + rand()%100)/100;
+		valorAtk = (double) podrDisp * efDisp;
+		podrDef = caracB.armadura * caracB.velocidad;
+		
+		DP =  ((double)(valorAtk - podrDef)/ (double)maxDP)*100;
+		
+		if (DP < 0) {
+			DP = 0;
+		}
+
+		pB->DatosPersonales->Salud -= DP;
+
+		printf("\nTurno #%d\n", i);
+		printf("%s ataca a %s\n", pA->DatosPersonales->ApellidoNombre, pB->DatosPersonales->ApellidoNombre);
+		printf("Poder de disparo: %d\n", podrDisp);
+		printf("Efectividad de disparo: %.2lf\n", efDisp);
+		printf("Valor de ataque: %.2lf\n", valorAtk);
+		printf("Defensa del enemigo: %d\n", podrDef);
+		printf("Danio producido: %.2lf\n", DP);
+		printf("Salud de %s: %.2lf\n", pB->DatosPersonales->ApellidoNombre, pB->DatosPersonales->Salud);
+	}
+
+
+	printf("\nSalud de %s: %.2lf\n", p1->DatosPersonales->ApellidoNombre, p1->DatosPersonales->Salud);
+	printf("\nSalud de %s: %.2lf\n", p2->DatosPersonales->ApellidoNombre, p2->DatosPersonales->Salud);
+
+	if (p1->DatosPersonales->Salud > p2->DatosPersonales->Salud) {
+		printf("\nGana %s", p1->DatosPersonales->ApellidoNombre);
+	}
+	else if (p1->DatosPersonales->Salud > p2->DatosPersonales->Salud){
+
+		printf("\nGana %s\n", p2->DatosPersonales->ApellidoNombre);
+
+	}
+	else {
+		printf("\nEmpate\n");
+		printf("p1:\n");
+		printf("p2:\n");
+	}
+
+	
+
 	return;
 }
